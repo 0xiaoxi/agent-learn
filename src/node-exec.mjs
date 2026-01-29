355462180b436dd1c340d59728726fa346243cca
0xiaoxi <xiaoxi.lin@jiyinbio.com>
2026-01-29 08:59:14 +0800

    npm init

--- src/node-exec.mjs
+++ src/node-exec.mjs
@@ -1,13 +1,13 @@
 import { spawn } from'node:child_process';
 
-const command = '"n`nn" | pnpm create vite react-todo-app --template react-ts';
+const command = 'pnpm create vite react-todo-app --template react-ts';
 // 获取当前工作目录
 const cwd = process.cwd();
 // 使用 spawn 创建子进程执行命令
 const child = spawn(command, {  
   cwd, // 设置子进程的工作目录  
   stdio: 'inherit', // 实时输出到控制台（继承父进程的 stdin, stdout, stderr）  
-  shell: 'powershell.exe'// 从 Node.js 18.17.0 和 20.0.0 开始，spawn 的 shell 选项可以传字符串});
+  shell: true,
 });
 let errorMsg = '';
 
